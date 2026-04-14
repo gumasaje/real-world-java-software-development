2f34627bceca0d424f03346be888d316aefe60ba
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-04-14 19:58:37 +0900

    refactor: OCP 원칙을 적용하여 검색 기능 확장성 확보

--- src/main/java/chapter_03/BankStatementProcessor.java
+++ src/main/java/chapter_03/BankStatementProcessor.java
@@ -40,6 +40,16 @@ public class BankStatementProcessor {
         return total;
     }
 
+    public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
+        final List<BankTransaction> result =  new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransactionFilter.test(bankTransaction)) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+/*
     public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
         final List<BankTransaction> result = new ArrayList<>();
         for (final BankTransaction bankTransaction : bankTransactions) {
@@ -69,4 +79,5 @@ public class BankStatementProcessor {
         }
         return result;
     }
+*/
 }
